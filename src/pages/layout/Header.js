94b364565b2321389b6df7b757b65d655ed45692
beombeom1119 <qjarl1119@naver.com>
2023-05-18 09:42:43 +0900

    "링크 수정"

--- src/pages/layout/Header.js
+++ src/pages/layout/Header.js
@@ -10,13 +10,13 @@ const Header = () => {
                 <Link to="/"><img className="logo" src={logo}/></Link>
             </div>{ }
             <div className="nav_area" styled={{ marginLeft: "60px"}}>
-                <div ><Link to="/payment" className="link">결제</Link></div>
-                <div><Link to="/ready" className="link">결제 준비</Link></div>
-                <div><Link to="/confirm" className="link">결제 승인</Link></div>
-                <div><Link to="/retrieve" className="link">결제 조회</Link></div>
-                <div><Link to="/cancel" className="link">결제 취소</Link></div>
-                <div><Link to="/verify" className="link">결제 검증</Link></div>
-                <div><Link to="/capture" className="link">수동매입</Link></div>
+                <div ><Link to="/demo/payment" className="link">결제</Link></div>
+                <div><Link to="/demo/ready" className="link">결제 준비</Link></div>
+                <div><Link to="/demo/confirm" className="link">결제 승인</Link></div>
+                <div><Link to="/demo/retrieve" className="link">결제 조회</Link></div>
+                <div><Link to="/demo/cancel" className="link">결제 취소</Link></div>
+                <div><Link to="/demo/verify" className="link">결제 검증</Link></div>
+                <div><Link to="/demo/capture" className="link">수동매입</Link></div>
             </div>
         </div>
     )
